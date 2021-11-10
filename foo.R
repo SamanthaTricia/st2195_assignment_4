8a385b4caee5f719ee2c2ecd9ec922c74c9bf512
Samantha <st003@mymail.sim.edu.sg>
2021-11-10 23:16:32 +0700

    Sam's 2nd commit

--- foo.R
+++ foo.R
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # Radius
 r<-2
 
@@ -14,5 +15,28 @@ volume_vector <- function(r){
   }
 }
 
+=======
+
+# Function to compute the volume of a sphere with radius r
+volume <- function(r) {
+  (4/3)*pi*(r^3)
+}
+
+# Function to compute the volumes of the spheres with radius r, r^2 and r^3
+volume_vector <- function(r) {
+  #r <- 22
+  volumes <- c()
+  for (power in 1:3){
+    vol <- volume(r^power)
+    #volumes <- c(volumes, vol)
+    volumes <- append(volumes, vol)
+  }
+  return(volumes)
+}
+
+# Radius
+r <- 2
+
+>>>>>>> e72fa77 (Sam's 2nd commit)
 # Run volume_vector(r) and print the volumes of the spheres with radius r, r^2 and r^3
 volume_vector(r)
